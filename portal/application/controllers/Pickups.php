87f530f4889815321df1256af24e521ab1f1514a
abdulrahman <abdulrahman.g.alabod@gmail.com>
2022-06-15 10:02:04 +0200

    move pickup into controller & refactor url

---
+++ portal/application/controllers/Pickups.php
@@ -0,0 +1,91 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+set_time_limit(0);
+
+class Pickups extends ClientsController
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        if (!isset($_SESSION['client_logged_in'])) {
+            header('Location: ' . site_url('authentication/login'));
+            exit;
+        }
+    }
+
+    public function tracking()
+    {
+
+
+        $this->title('Page Title');
+        $this->view('helloo');
+        $this->layout();
+    }
+
+    public function partners()
+    {
+        $this->title('Partners');
+        $this->view('partners');
+        $this->layout();
+    }
+
+    public function ask()
+    {
+
+        $this->title('Ask a lawyer');
+        $this->view('ask');
+        $this->layout();
+    }
+
+    public function request_service()
+    {
+        $this->title('Request Service');
+        $this->view('request_service');
+        $this->layout();
+    }
+
+    public function help()
+    {
+        $this->title('Help');
+        $this->view('help');
+        $this->layout();
+    }
+
+    public function send_documents()
+    {
+        $this->title('Send Documents');
+        $this->view('send_documents');
+        $this->layout();
+    }
+
+    public function start_company()
+    {
+        $this->title('Start a Company');
+        $this->view('start_company');
+        $this->layout();
+    }
+
+    public function make_contract()
+    {
+        $this->title('Make a Contract');
+        $this->view('make_contract');
+        $this->layout();
+    }
+
+    public function incorporation()
+    {
+        $this->title('Online Incorporation');
+        $this->view('online_incorporation');
+        $this->layout();
+    }
+
+    public function incorporation_egypt()
+    {
+        $this->title('Egypt Online Incorporation');
+        $this->view('egypt_online_incorporation');
+        $this->layout();
+    }
+}
--- portal/application/views/themes/perfex/sidebar.php
+++ portal/application/views/themes/perfex/sidebar.php
@@ -80,7 +80,7 @@
           </ul>
         </li>
         <li class="nav-item pcoded-hasmenu">
-          <a href="<?php echo site_url('pickup/pickups/request_service'); ?>" class="nav-link justify-content-between">
+          <a href="<?php echo site_url('pickups/request_service'); ?>" class="nav-link justify-content-between">
             <span class="pcoded-micon">
               <img src="<?php echo base_url('assets/images/main/requestservice.svg"') ?>"alt="requestservice">
               <span class="pcoded-mtext">Request service</span>
@@ -91,7 +91,7 @@
        
         </li>
         <li class="nav-item pcoded-hasmenu">
-          <a href="<?php echo site_url('pickup/pickups/ask'); ?>" class="nav-link justify-content-between">
+          <a href="<?php echo site_url('pickups/ask'); ?>" class="nav-link justify-content-between">
             <span class="pcoded-micon">
               <img src="<?php echo base_url('assets/images/main/askalaywer.svg"') ?>"alt="askalaywer">
               <span class="pcoded-mtext">Ask a Lawyer</span>
