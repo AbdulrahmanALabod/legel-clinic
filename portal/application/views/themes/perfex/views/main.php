95ae8266f07f994dcbea8ec15e316ed7e7dee6f1
abdulrahman <abdulrahman.g.alabod@gmail.com>
2022-06-22 16:26:17 +0200

    replace disable by hidden

--- portal/application/views/themes/perfex/views/main.php
+++ portal/application/views/themes/perfex/views/main.php
@@ -134,7 +134,7 @@
                   <p>SPECIALIZED IN STARTUPS & <br>ENTREPRENEURSHIP IN THE MENA <br>REGION</p>
                   <div class="row d-flex pt-3 justify-content-between">
                     <div class="col-12 col-sm-6">
-                      <button <?php echo isset($_SESSION['client_logged_in']) ? 'disabled' : ''?>  class="btn btn-sub" onclick="location.href='<?php echo base_url('authentication/login');?>';" type="button">Subscribe</button>
+                      <button <?php echo isset($_SESSION['client_logged_in']) ? 'hidden' : ''?>  class="btn btn-sub" onclick="location.href='<?php echo base_url('authentication/login');?>';" type="button">Subscribe</button>
                     </div>
                     <div class="col-12 col-sm-6">
                       <button onclick="location.href='<?php echo base_url('pickups/ask');?>';" class="btn btn-ask" type="button">Ask Lawyer</button>
