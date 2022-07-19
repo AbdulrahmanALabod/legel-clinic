92127a6e475512574b4c87b7a7dc1e4e9be88ca2
abeer26omar <abeer.omar504@gmail.com>
2022-07-19 10:56:00 +0200

    add id

--- portal/application/views/form/index_ar.php
+++ portal/application/views/form/index_ar.php
@@ -342,7 +342,7 @@
                                 </a>
                             </div>
                         </div>
-                            <div class="pricing-365">
+                            <div class="pricing-365" id="price_365">
                                 <div class="container">
                                     <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3 g-4 justify-content-center">
                                         <div class="col">
